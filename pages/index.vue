f4ae8b0ecb65aeac6e9a648ed2f9b18fdbc7649d
AdrianCeku <cekuadrian4@gmail.com>
2024-01-04 02:25:53 +0100

    last commit

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
     <div class="container mx-auto grid place-items-center">
         <h1 class="text-4xl font-bold mt-4 mb-10">Welcome to <span class="text-yellow-400 font-extrabold">Brand Store</span> 😱🔥</h1>
         <p class="mb-10"> This is an example project</p>
+
         <div class="grid gap-4 grid-flow-col grid-rows-3 xl:grid-cols-3 xl:grid-rows-1 " >
             <div class="grid justify-items-center w-300">
                 <img class="rounded-lg max-h-64 object-fill hover:scale-105 transition" src="https://media.istockphoto.com/id/1138429558/photo/rows-of-shelves.jpg?s=1024x1024&w=is&k=20&c=bMRYi_ay8T0EtezBRkK-JJ98LpgcC8o6ri2Ixo-1l4c=">
@@ -22,15 +23,13 @@
 
         <div class="text-center m-10 text-xl max-w-5xl mx-auto font-400">
             <p class=""> 
-                Welcome to <span class="text-yellow-400">Brand Store 😱🔥</span> where we've mastered the art of selling things! At our online emporium, we offer a curated selection of items that you didn't know you needed until you stumbled upon our website. <br> Our store is so generic that we've even surprised ourselves with the sheer averageness of our products.<br>
-
-                <br>Picture this: a virtual shelf filled with products that may or may not change your life in the most inconspicuous ways. From "Generic Brand T-Shirts" to "Essential Things You Probably Already Own," we've got it all. Our limited edition collection includes such classics as <span class="text-yellow-400">Unbranded Water Bottles</span> and <span class="text-yellow-400">Plain White Socks</span>.<br>
-
-                <br>Don't be fooled by our lack of uniqueness; we take pride in providing you with the things you never knew you were looking for. Whether you're searching for the perfect gift for someone you vaguely know or just want to treat yourself to something ordinary, <span class="text-yellow-400">Brand Store 😱🔥</span> is the place to be. Embrace the mediocrity, revel in the simplicity, and join us on a journey of unremarkable shopping experiences.  
-                <br><br><span class="text-yellow-400 text-2xl font-bold">Because when it comes to being average, we're exceptionally good at it!</span>
+                <p>Welcome to <span class="text-yellow-400">Brand Store 😱🔥</span> where we've mastered the art of selling things! At our online emporium, we offer a curated selection of items that you didn't know you needed until you stumbled upon our website. <br> Our store is so generic that we've even surprised ourselves with the sheer averageness of our products.</p>
+                <p><br>Picture this: a virtual shelf filled with products that may or may not change your life in the most inconspicuous ways. From "Generic Brand T-Shirts" to "Essential Things You Probably Already Own," we've got it all. Our limited edition collection includes such classics as <span class="text-yellow-400">Unbranded Water Bottles</span> and <span class="text-yellow-400">Plain White Socks</span>.</p>
+                <p><br>Don't be fooled by our lack of uniqueness; we take pride in providing you with the things you never knew you were looking for. Whether you're searching for the perfect gift for someone you vaguely know or just want to treat yourself to something ordinary, <span class="text-yellow-400">Brand Store 😱🔥</span> is the place to be. Embrace the mediocrity, revel in the simplicity, and join us on a journey of unremarkable shopping experiences.</p>  
+                <p><br><span class="text-yellow-400 text-2xl font-bold">Because when it comes to being average, we're exceptionally good at it!</span></p>
             </p>
         </div>
-
+        
         <p class="m-10 text-3xl font-medium text-center bg-yellow-400 rounded-lg p-2 hover:scale-105 hover:bg-yellow-300 transition">
             <NuxtLink to="/products">Take a look at our Products</NuxtLink>
         </p>
